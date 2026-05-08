09c8790d88c0d108253e5809edb2e4137e9d8925
Angelica1234567891 <angelica.solveig@icloud.com>
2026-05-08 10:15:35 +0200

    ferdig med html?

--- watermelon_drop/script.js
+++ watermelon_drop/script.js
@@ -1,3 +1,19 @@
+const burger = document.getElementById("burger")
+const kryss = document.getElementById("kryss")
+const navbar = document.getElementById("navbar")
+
+burger.addEventListener("click", function() {
+    navbar.classList.add("open")
+    burger.style.display = "none"
+    kryss.style.display = "block"
+})
+
+kryss.addEventListener("click", function() {
+    navbar.classList.remove("open")
+    burger.style.display = "block"
+    kryss.style.display = "none"
+})
+
 
 
 const canvas = document.querySelector("canvas")
@@ -7,15 +23,12 @@ canvas.width = canvas.offsetWidth //regner ut css width: 730px
 canvas.height = canvas.offsetHeight // regner ut høyden: 200px
 
 
-
 // konteksten (som vi bruker til å tegne med):
 const ctx = canvas.getContext("2d")
 
 let x = 100
 let y = 100
-let vx = 4
-let vy = 5
-const radius = 40
+let radius = 40
 const g = 0.1
 let farge = "red"
 
@@ -33,41 +46,6 @@ canvas.addEventListener("click", function (e) {
 
     farge = `hsl(${Math.random() * 360}, 100%, 50% )`
     radius = Math.floor(Math.random() * 20) + 10
-    tegnSirkel(x, y, radius, farge)
+    tegnBall(x, y, radius, farge)
 })
 
-function oppdaterBall() {
-    vy += g
-    x += vx
-    y += vy
-
-    if (x < radius || x > (WIDTH - radius)) {
-        //snu farts-retningen:
-        vx *= -1
-        farge = `hsl(${Math.random() * 360}, 100%, 50% )`
-
-    }
-
-    if (y > (HEIGHT - radius)) {
-        //snu farts-retningen:
-        vy *= -1
-        //Og! Få oss tilbake til "bakkenivå":
-        y = (HEIGHT - radius)
-        farge = `hsl(${Math.random() * 360}, 100%, 50% )`
-    }
-}
-
-function loop() {
-    oppdaterBall()
-
-    //clear canvas:
-    ctx.clearRect(0, 0, WIDTH, HEIGHT)
-
-    ctx.fillStyle = "rgba(173, 216, 230, 0.1)"
-
-    tegnBall()
-    requestAnimationFrame(loop)
-}
-
-// Start loopen vår:
-loop()
